a019c73aa7902bdf7d0b1e9a27732de6cdafed52
Bill Lillyman <wbl@tuta.io>
2021-04-13 14:28:28 -0400

    settings clean

--- erp_backend/settings.py
+++ erp_backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!bnm6%@pc#0k!a&gq29_sfr5)wko74&suf(zp_8*!mr%_!olm5'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
